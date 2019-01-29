675fb664c11ec3a0dcb1440863f0e583493ace9a
webdevabhi <abhishek.patel185@gmail.com>
2019-01-29 16:25:29 +0530

    Passing ingredients from recipeDetail to shopping list

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,7 +1,9 @@
 import { Recipe } from './recipe.modal';
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
     recipes: Recipe[] = [
@@ -25,7 +27,13 @@ export class RecipeService {
         )
     ];
 
+    constructor(private slService: ShoppingListService) {}
+
     getRecipes() {
         return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingretients: Ingredient[]) {
+        this.slService.addIngredients(ingretients);
     }
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -17,4 +17,9 @@ export class ShoppingListService {
     this.ingredients.push(ingredient);
     this.ingredientChanged.emit(this.ingredients.slice());
   }
+
+  addIngredients(ingredients: Ingredient[]) {
+    this.ingredients.push(...ingredients);
+    this.ingredientChanged.emit(this.ingredients.slice());
+  }
 }
