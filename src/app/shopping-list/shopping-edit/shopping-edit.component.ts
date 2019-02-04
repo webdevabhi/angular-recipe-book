13650a514d99cc0af3735aa449e61e4e4edf8566
webdevabhi <abhishek.patel185@gmail.com>
2019-02-04 16:20:13 +0530

    change the function nae

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -29,7 +29,7 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     );
   }
 
-  onAddItem(form: NgForm) {
+  onSubmitForm(form: NgForm) {
     const value = form.value;
 
     const newIngredient = new Ingredient(value.name, value.amount);
