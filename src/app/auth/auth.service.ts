0fe72a0c7871207a35bfb021ce83d5fc7a2dd315
webdevabhi <abhishek.patel185@gmail.com>
2019-02-05 14:16:14 +0530

    siging users in

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -9,4 +9,14 @@ export class AuthService {
                 error => console.log(error)
             );
     }
+
+    signinUsers(email: string, password: string) {
+        firebase.auth().signInWithEmailAndPassword(email, password)
+            .then(
+                response => console.log(response)
+            )
+            .catch(
+                error => console.log(error)
+            );
+    }
 }
